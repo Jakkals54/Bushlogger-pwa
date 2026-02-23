fbc55d1360e8ad23b46bcf0202d6fa73f253c2c7
Jakkals54 <125199288+Jakkals54@users.noreply.github.com>
2026-02-23 17:33:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ const BushloggerApp = (() => {
         elements.checklistSearch = document.getElementById("checklistSearch");
         
 });
-    }
+    
 
     // ------------------------ Local Storage ------------------------
     function loadFromStorage() {
