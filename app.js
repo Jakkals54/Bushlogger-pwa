3011f256a1ccb87bac4912994877c06cdec8ea8d
Jakkals54 <125199288+Jakkals54@users.noreply.github.com>
2026-02-27 10:13:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ const BushloggerApp = (() => {
         const observer = observerOverride || observerValue || "Guest";
 
 // Only update memory if manually typed or changed
-if (observer && observer !== "Guest") {
+    if (observer && observer !== "Guest") {
 
     // Remove if exists
     state.observers = state.observers.filter(name => name !== observer);
